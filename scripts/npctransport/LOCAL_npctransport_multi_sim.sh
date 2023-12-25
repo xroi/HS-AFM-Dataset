20bf28af021fb7685c89ef5635d66a053b266a3a
xroi <40072297+xroi@users.noreply.github.com>
2023-12-25 15:01:54 +0200

    Updated & added scripts

--- scripts/npctransport/LOCAL_npctransport_multi_sim.sh
+++ scripts/npctransport/LOCAL_npctransport_multi_sim.sh
@@ -12,11 +12,11 @@ length=$(jq length ${1})
 for ((i=0; i<length; i++))
 do
   amount=$(jq ".[${i}].amount" ${1})
-  config_path=$(jq ".[${i}].config_path" ${1})
-  output_path=$(jq ".[${i}].output_path" ${1})
+  config_path=$(jq -r ".[${i}].config_path" ${1})
+  output_path=$(jq -r ".[${i}].output_path" ${1})
   step=$(jq ".[${i}].step" ${1})
   stop=$(jq ".[${i}].stop" ${1})
   output_statistics_interval=$(jq ".[${i}].output_statistics_interval" ${1})
 
-  sbatch --array=1-$((${amount}/2)) ${SCRIPTS_FOLDER}/npctransport/LOCAL_npctransport_sequential_batch.sh ${output_path} ${config_path} ${step} ${output_statistics_interval} ${stop}
+  sbatch --array=1-$((${amount}/2)) --output=${output_path}/slurm ${SCRIPTS_FOLDER}/npctransport/LOCAL_npctransport_sequential_batch.sh ${output_path} ${config_path} ${step} ${output_statistics_interval} ${stop}
 done
