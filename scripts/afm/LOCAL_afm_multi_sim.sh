6e5620c4f94b741c852cbce968ab0871baafb3d0
xroi <40072297+xroi@users.noreply.github.com>
2023-12-25 14:41:54 +0200

    Updated & added scripts

--- scripts/afm/LOCAL_afm_multi_sim.sh
+++ scripts/afm/LOCAL_afm_multi_sim.sh
@@ -11,10 +11,10 @@ length=$(jq length ${1})
 
 for ((i=0; i<length; i++))
 do
-  amount=$(jq `.[${i}].amount` ${1})
-  input_path=$(jq `.[${i}].input_path` ${1})
-  output_path=$(jq `.[${i}].output_path` ${1})
-  config_path=$(jq `.[${i}].config_path` ${1})
+  amount=$(jq '.[${i}].amount' ${1})
+  input_path=$(jq '.[${i}].input_path' ${1})
+  output_path=$(jq '.[${i}].output_path' ${1})
+  config_path=$(jq '.[${i}].config_path' ${1})
 
   sbatch --array=1-$((${amount}/2)) ${SCRIPTS_FOLDER}/afm/LOCAL_afm_batch.sh ${input_path} ${output_path} ${config_path}
 done
