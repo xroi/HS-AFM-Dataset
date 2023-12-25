f2ca67a80247c3326588c956533e73406b89d6a0
xroi <40072297+xroi@users.noreply.github.com>
2023-12-25 14:51:39 +0200

    Updated & added scripts

--- scripts/npctransport/LOCAL_npctransport_multi_sim.sh
+++ scripts/npctransport/LOCAL_npctransport_multi_sim.sh
@@ -16,7 +16,7 @@ do
   output_path=$(jq ".[${i}].output_path" ${1})
   step=$(jq ".[${i}].step" ${1})
   stop=$(jq ".[${i}].stop" ${1})
-  output_statistics_interval=$(jq '.[${i}].output_statistics_interval' ${1})
+  output_statistics_interval=$(jq ".[${i}].output_statistics_interval" ${1})
 
   sbatch --array=1-$((${amount}/2)) ${SCRIPTS_FOLDER}/npctransport/LOCAL_npctransport_sequential_batch.sh ${output_path} ${config_path} ${step} ${output_statistics_interval} ${stop}
 done
